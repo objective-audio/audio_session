ade5423b2423b9b9c535f5aa6169b99d338188ff
Yuki Yasoshima <yukiyasos@gmail.com>
2020-01-01 15:36:22 +0900

    mode selection

--- AudioSession/CategoriesViewController.swift
+++ AudioSession/CategoriesViewController.swift
@@ -2,6 +2,11 @@ import UIKit
 import Chaining
 
 class CategoriesViewController: UITableViewController {
+    enum Section: Int, CaseIterable {
+        case category
+        case mode
+    }
+    
     weak var controller: SessionController?
     private var pool = ObserverPool()
     
@@ -10,37 +15,71 @@ class CategoriesViewController: UITableViewController {
         
         guard let controller = self.controller else { return }
         
-        let observer = controller.category.chain().do { [weak self] _ in
-            self?.tableView.reloadSections(.init(integer: 0), with: .fade)
-        }.sync()
+        controller.category.chain().do { [weak self] _ in
+            self?.tableView.reloadSections(.init(integer: Section.category.rawValue), with: .fade)
+        }.sync().addTo(self.pool)
         
-        self.pool.add(observer)
+        controller.mode.chain().do { [weak self] _ in
+            self?.tableView.reloadSections(.init(integer: Section.mode.rawValue), with: .fade)
+        }.sync().addTo(self.pool)
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return Section.allCases.count
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Categories"
+        guard let section = Section(rawValue: section) else { fatalError() }
+        
+        switch section {
+        case .category:
+            return "Category"
+        case .mode:
+            return "Mode"
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Category.allCases.count
+        guard let section = Section(rawValue: section) else { fatalError() }
+        
+        switch section {
+        case .category:
+            return Category.allCases.count
+        case .mode:
+            return Mode.allCases.count
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let section = Section(rawValue: indexPath.section) else { fatalError() }
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
         
-        let category = Category.allCases[indexPath.row]
-        cell.textLabel?.text = category.name
-        cell.accessoryType = (self.controller?.category.value == category) ? .checkmark : .none
+        switch section {
+        case .category:
+            let category = Category.allCases[indexPath.row]
+            cell.textLabel?.text = category.name
+            cell.accessoryType = (self.controller?.category.value == category) ? .checkmark : .none
+        case .mode:
+            let mode = Mode.allCases[indexPath.row]
+            cell.textLabel?.text = mode.name
+            cell.accessoryType = (self.controller?.mode.value == mode) ? .checkmark : .none
+        }
+        
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let category = Category.allCases[indexPath.row]
-        self.controller?.category.value = category
+        guard let section = Section(rawValue: indexPath.section) else { fatalError() }
+        
+        switch section {
+        case .category:
+            let category = Category.allCases[indexPath.row]
+            self.controller?.category.value = category
+        case .mode:
+            let mode = Mode.allCases[indexPath.row]
+            self.controller?.mode.value = mode
+        }
     }
 }
