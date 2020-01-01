bedb075c258dcdf45a49f853f2c736116c7b06e7
Yuki Yasoshima <yukiyasos@gmail.com>
2020-01-01 15:46:05 +0900

    activate deactivate

--- AudioSession/SessionController.swift
+++ AudioSession/SessionController.swift
@@ -1,7 +1,19 @@
 import Foundation
+import AVFoundation
 import Chaining
 
 class SessionController {
     let category = ValueHolder<Category>(.playback)
     let mode = ValueHolder<Mode>(.default)
+    
+    private var session: AVAudioSession { return AVAudioSession.sharedInstance() }
+    
+    func activate() {
+        try? self.session.setCategory(self.category.value.sessionCategory)
+        try? self.session.setActive(true, options: [])
+    }
+    
+    func deactivate() {
+        try? self.session.setActive(false, options: [])
+    }
 }
