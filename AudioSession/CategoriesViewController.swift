12b926c523e81642302eb8170b07d592e485ad8c
Yuki Yasoshima <yukiyasos@gmail.com>
2020-01-01 14:52:51 +0900

    Select Category

--- AudioSession/CategoriesViewController.swift
+++ AudioSession/CategoriesViewController.swift
@@ -1,8 +1,38 @@
 import UIKit
 
 class CategoriesViewController: UITableViewController {
+    weak var controller: SessionController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Categories"
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Category.allCases.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        let category = Category.allCases[indexPath.row]
+        cell.textLabel?.text = category.name
+        cell.accessoryType = (self.controller?.category == category) ? .checkmark : .none
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let category = Category.allCases[indexPath.row]
+        self.controller?.category = category
+    }
 }
-
--- AudioSession/SceneDelegate.swift
+++ AudioSession/SceneDelegate.swift
@@ -1,14 +1,20 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
+    
+    let controller = SessionController()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        guard let window = self.window else { return }
+        guard let navigation = window.rootViewController as? UINavigationController else { return }
+        guard let categories = navigation.topViewController as? CategoriesViewController else { return }
+        
+        categories.controller = self.controller
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
