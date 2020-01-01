43df8f51faa8fd341c8b7a33357de0cb7d49ca7b
Yuki Yasoshima <yukiyasos@gmail.com>
2020-01-01 15:16:16 +0900

    category default

--- AudioSession/SessionController.swift
+++ AudioSession/SessionController.swift
@@ -2,5 +2,6 @@ import Foundation
 import Chaining
 
 class SessionController {
-    let category = ValueHolder<Category>(.playback)
+    let category = ValueHolder<Category>(.ambient)
+    let mode = ValueHolder<Mode>(.default)
 }
